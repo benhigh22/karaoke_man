016bc6f83cea2c6a5fb797800c3398d406f1f4c8
benhigh22 <benjaming.high@gmail.com>
2016-03-31 15:30:54 -0400

    Added models

--- karaoke_man_app/models.py
+++ karaoke_man_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    score = models.IntegerField()
+    preferred_city = models.ForeignKey(City)
+
+
+class Party(models.Model):
+    city = models.ForeignKey(City)
+    location_name = models.CharField(max_length=100)
+    street_address = models.CharField(max_length=200)
+    theme = models.CharField(max_length=100, blank=True)
+
+
+class Song(models.Model):
+    song_name = models.CharField(max_length=100)
+    user = models.ManyToManyField(User)
