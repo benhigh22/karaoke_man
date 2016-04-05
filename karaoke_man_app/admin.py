fad1b365fa1912638deacff3fb95967222edd475
benhigh22 <benjaming.high@gmail.com>
2016-04-05 10:44:32 -0400

    Added to admin

--- karaoke_man_app/admin.py
+++ karaoke_man_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from karaoke_man_app.models import UserProfile, City, Party, Song
 
-from karaoke_man_app.models import City, Party
 
+admin.site.register(UserProfile)
 admin.site.register(City)
 admin.site.register(Party)
+admin.site.register(Song)
