14787948f9c0dd7a99e40c8f71dd11046676668d
benhigh22 <benjaming.high@gmail.com>
2016-04-11 09:56:52 -0400

    Added __str__

--- karaoke_man_app/models.py
+++ karaoke_man_app/models.py
@@ -35,6 +35,9 @@ class Party(models.Model):
     party_name = models.CharField(max_length=75)
     desription = models.TextField(null=True, blank=True)
 
+    def __str__(self):
+        return self.party_name
+    
 
 class Queue(models.Model):
     singer_name = models.CharField(max_length=100)
