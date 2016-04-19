a1581df8ab68345b4b749251178486a94bd0dbc1
benhigh22 <benjaming.high@gmail.com>
2016-04-19 10:19:44 -0400

    About to begin about us page

--- karaoke_man/urls.py
+++ karaoke_man/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
     url(r'^api/cities/(?P<city>\d+)/$', views.CityRetrieveUpdateDestroyAPIView.as_view(), name='city_retrieve_update_destroy'),
     url(r'^api/cities/(?P<city>\d+)/locations/(?P<pk>\d+)/$', views.LocationRetrieveUpdateDestroyAPIView.as_view(), name='location_retrieve_update_destroy'),
     url(r'^api/locations/(?P<location>\d+)/parties/(?P<pk>\d+)/$', views.PartyRetrieveUpdateDestroyAPIView.as_view(), name='party_retrieve_update_destroy'),
-    # Joes url below
     url(r'^api/cities/(?P<city>\d+)/parties/$', views.AllCitiesPartiesListAPIView.as_view(), name='list_all_parties_api_view'),
     url(r'^api/parties/(?P<party>\d+)/attendees/(?P<pk>\d+)/$', views.AttendeeRetrieveUpdateDestroyAPIView.as_view(), name='attendee_retrieve_update_destroy'),
     url(r'^api/users/(?P<user>\d+)/attendees/(?P<pk>\d+)/$', views.UserAttendeeRetrieveUpdateDestroyAPIView.as_view(), name='user_attendee_retrieve_update_destroy'),
